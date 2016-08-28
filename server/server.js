78c47e328c1fdc04affca3e0239d103ecc2a4dc5
梁伟 <liangwei@qiangpiaoyi.com>
2016-08-28 17:21:04 +0800

    更改一下目录结构

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config.dev.js');
+var config = require('../webpack.config.dev.js');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
